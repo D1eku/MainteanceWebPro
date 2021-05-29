52cd6ab1f949e5484fe4815824f3e04fe7636643
Mauro Olivares Sierra <43655114+mauroolivares@users.noreply.github.com>
2021-05-29 15:50:38 -0400

    probando desde el inicio lol

--- public/routes/index.js
+++
@@ -1,9 +0,0 @@
-const { Router } = require('express');
-const router = Router();
-module.exports = router;
-
-router.get('/users', (req, res) => {
-    res.send('users')
-});
-
-module.exports = router;
