3ab5303ea65f4389b213421ebd23a12b8688d10c
Mauro Olivares Sierra <43655114+mauroolivares@users.noreply.github.com>
2021-06-08 02:02:37 -0400

    "UnhandledPromiseRejectionWarning: error: la autentificaci�n password fall� para el usuario �Mauro�"
    Estupido error.

--- src/js/dbconnect.js
+++ src/js/dbconnect.js
@@ -1,3 +1,20 @@
+const { Client } = require('pg');
+
+const client = new Client({
+    connectionString: process.env.DATABASE_URL,
+    ssl: false
+});
+
+client.connect();
+
+client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
+    if (err) throw err;
+    for (let row of res.rows) {
+        console.log(JSON.stringify(row));
+    }
+    client.end();
+});
+/*
 const { Pool } = require('pg');
 
 const pool = new Pool({
@@ -5,10 +22,10 @@ const pool = new Pool({
     user: 'zaeefiudjurtxf',
     password: '13fe4c1be4f86edf36839d648b3ec614c59936bb4afcde71223dce8a772ce341',
     database: 'd5abtmg5q8j2g7',
-    port: '5432',
-    ssl: true
+    port: '5432'
 });
+*/
 
 module.exports = {
-    pool
+    client
 }
