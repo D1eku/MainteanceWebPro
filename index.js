6bdf2a1601195c86097664a219a37da69f4a71b0
Mauro Olivares Sierra <43655114+mauroolivares@users.noreply.github.com>
2021-06-08 14:11:13 -0400

    Test?

--- index.js
+++ index.js
@@ -6,15 +6,16 @@ const port = 3000;
 const { Client } = require("pg");
 
 const client = new Client({
-  user: "zaeefiudjurtxf",
-  host: "ec2-34-193-112-164.compute-1.amazonaws.com",
-  database: "d5abtmg5q8j2g7",
-  password: "13fe4c1be4f86edf36839d648b3ec614c59936bb4afcde71223dce8a772ce341",
-  port: 5432,
+    user: "zaeefiudjurtxf",
+    host: "ec2-34-193-112-164.compute-1.amazonaws.com",
+    database: "d5abtmg5q8j2g7",
+    password: "13fe4c1be4f86edf36839d648b3ec614c59936bb4afcde71223dce8a772ce341",
+    port: 5432,
 });
 
 client.connect();
-//Conexion SQL
+console.log("???? hola probando algo")
+    //Conexion SQL
 
 
 //Express
@@ -22,7 +23,7 @@ const express = require('express');
 const app = express();
 app.use(require('/routes/index'))
 app.listen(port, function() {
-  console.log('Servidor web escuchando en el puerto 3000');
+    console.log('Servidor web escuchando en el puerto 3000');
 });
 
 
@@ -31,18 +32,15 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 //Gets 
-app.get("/", function(req, res){
-  console.log("GET --> /")
-  res.sendFile("/index.html")
+app.get("/", function(req, res) {
+    console.log("GET --> /")
+    res.sendFile("/index.html")
 });
 
 
 //Post
-app.post("/try-login", function(req, res){
-  console.log("trying to loggin ")
-  console.log(req.body)
+app.post("/try-login", function(req, res) {
+    console.log("trying to loggin ")
+    console.log(req.body)
 
 });
-
-
-
