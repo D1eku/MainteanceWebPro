b42aaf14168a617fb23a2fd67c47fd1a18775c38
D1eku <dieku25ikari@gmail.com>
2021-07-13 03:50:19 -0400

    menu mantenedor y rellenar ficha mantencion

--- src/controllers/mantenedor.controllers.js
+++ src/controllers/mantenedor.controllers.js
@@ -1,4 +1,5 @@
 const { pool } = require('../js/dbconnect');
+
 /*
 const addNewPauta = async (req,res) => {
     try{
@@ -17,6 +18,20 @@ revisarPosGPS
 
 */
 
-module.exports = {
+const mantenedorMenu = async(req, res) => {
+    try {
+        //Mira la consola 
+        //console.log(req.body);
+        // Por ahora las preubas se realizaran sobre dieku
+        // Por lo tanto 
+        const mantData = (await pool.query('select * from mantenedor m inner join empresa e on m.empresa = e.codigo where rut = "dieku"')).rows
+        res.render('mantenedor', {mantData})
+    } catch (err) {
+        console.log(err);
+    }
 
+}
+
+module.exports = {
+    mantenedorMenu
 }
