9b8d135d1565ddd3f8bf8cd07024216eb539c3ac
cecirojas <cecilia.rojas@alumnos.ucn.cl>
2021-07-21 18:16:48 -0400

    avances n+2

--- src/controllers/planner.controllers.js
+++ src/controllers/planner.controllers.js
@@ -19,7 +19,7 @@ const planifMenu = async(req, res) => {
     try {
         //select fa.codigo_pauta, fa.fecha_estimada, pm.codigo_equipo, pm.rutMantenedor from equipo
         const pautas = (await pool.query("select pm.codigo, pm.nombre, pm.clase, pm.fecha_creacion, e.nombre nom_empresa, e.codigo cod_empresa, pm.equipo, pm.version, pm.estado from pauta_mantenimiento pm inner join empresa e on pm.empresa = e.codigo")).rows;
-        const calendarios = (await pool.query("select * from calendario"))
+        const calendarios = (await pool.query("select * from calendario")).rows
         res.render('planificador', { nombre: req.session.nombre, pautas, calendarios });
     } catch (err) {
         console.log(err);
