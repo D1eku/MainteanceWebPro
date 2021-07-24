8a2bc883af8c628ac12c9dfeb863a5772fa727e0
Mauro Olivares Sierra <43655114+mauroolivares@users.noreply.github.com>
2021-07-24 16:36:21 -0400

    Planificador listo
    - Añadir calendarizaciones para mantenedores listo
    - Creo que solo faltaría limpiar el código o hacer uno que otro cambio estético

--- src/controllers/planner.controllers.js
+++ src/controllers/planner.controllers.js
@@ -21,7 +21,6 @@ const planifMenu = async(req, res) => {
         const pautas = (await pool.query("select pm.codigo, pm.nombre, pm.clase, pm.fecha_creacion, e.nombre nom_empresa, e.codigo cod_empresa, pm.equipo, pm.version, pm.estado from pauta_mantenimiento pm inner join empresa e on pm.empresa = e.codigo")).rows;
         const calendarios = (await pool.query("select * from calendario")).rows
         const mantenedores = (await pool.query("select m.rut, u.nombre, m.rol, m.empresa from mantenedor  m  inner join usuario u on m.rut = u.rut where u.activo = 'Activado' ")).rows
-        console.log(mantenedores)
         res.render('planificador', { nombre: req.session.nombre, pautas, calendarios, mantenedores });
     } catch (err) {
         console.log(err);
@@ -34,10 +33,10 @@ const sendAssignDatePauta = async(req, res) => {
         //select fa.codigo_pauta, fa.fecha_estimada, pm.codigo_equipo, pm.rutMantenedor from equipo
         let {
             codigo_pauta,
-            name_pauta, //rutMantenedorXD,
+            name_pauta,
+            rut_mantenedor,
             fecha_estimada_pauta
         } = req.body;
-
         let codigo_calendario = Math.floor(Math.random() * 100000);
         var codExiste = (await pool.query('SELECT * FROM calendario WHERE codigo = $1', [codigo_calendario])).rows.length;
         var strfechaEst = fecha_estimada_pauta.split('-');
@@ -45,17 +44,14 @@ const sendAssignDatePauta = async(req, res) => {
 
         console.log("Tipo fecha: " + fEstimada + "; Tipo String: " + fecha_estimada_pauta + "\n")
 
-
         if (codExiste < 1) {
             const pautaActual = (await pool.query("select * from pauta_mantenimiento where codigo = $1", [codigo_pauta])).rows[0];
-            //const insertCalendario = await pool.query('insert into calendario values($1, $2, $3, $4, $5, $6', [codigo_calendario, pautaActual.equipo, "RutMantenedor", fEstimada, codigo_pauta, req.session.rut]);
+            const insertCalendario = await pool.query('insert into calendario values($1, $2, $3, $4, $5, $6)', [codigo_calendario, pautaActual.equipo, rut_mantenedor, fEstimada, codigo_pauta, req.session.rut]);
             res.redirect("/pln")
         } else {
             console.log("Ya existe un calendario con este código");
             res.redirect("/pln")
         }
-
-
 
 
     } catch (err) {
